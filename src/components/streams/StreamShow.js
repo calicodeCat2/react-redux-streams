d99da3d1c860139297ee1be479fee697de335374
David Murcek <dfmurcek@gmail.com>
2019-04-22 15:46:31 -0700

    Complete streamy app with rtmp server and ability to stream live video from OBS.

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -10,8 +10,34 @@ class StreamShow extends React.Component {
     this.videoRef = React.createRef();
   }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params
+    
+    this.props.fetchStream(id);
+    this.buildPlayer(); 
   }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Oh, bad show all around.</div>;
@@ -21,7 +47,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video ref={this.videoRef} />
+        <video ref={this.videoRef} style={{width: '100%'}} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
